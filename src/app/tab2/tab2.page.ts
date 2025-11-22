d7e2ad5e23076d78e3b9005f1874201c2febabf1
maximedrouault <maximedrouault@gmail.com>
2025-11-22 13:12:02 +0100

    feat: implement photo gallery functionality with camera integration

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {PhotoService, UserPhoto} from "../services/photo.service";
+import {ActionSheetController} from "@ionic/angular";
 
 @Component({
   selector: 'app-tab2',
@@ -8,6 +10,41 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  // CHANGE: Update constructor to include `photoService`
+  constructor(public photoService: PhotoService,
+              public actionSheetController: ActionSheetController) {}
 
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
+
+  // CHANGE: Add `addNewToGallery()` method
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+
+  public async showActionSheet(photo: UserPhoto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => {
+            this.photoService.deletePhoto(photo, position);
+          },
+        },
+        {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            // Nothing to do, action sheet is automatically closed
+          },
+        },
+      ],
+    });
+    await actionSheet.present();
+  }
 }
